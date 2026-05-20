39db87491b30c61c40df1cfd05f64aa4db47b39d
omsenpaiii <omtomar2004.ot@gmail.com>
2026-05-20 23:08:18 +0530

    fix: add missing Link import to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { AnimatePresence, motion, useScroll, useTransform } from "framer-motion";
 import {
   ArrowRight,
